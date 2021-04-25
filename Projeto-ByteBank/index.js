1781452ff17f990cdfd420c00d797e94013aa450
Fabio Devigilli <fabio.devigilli@hbsis.com.br>
2021-04-25 16:36:11 -0300

    Capitulo 02 finalizado

--- Projeto-ByteBank/index.js
+++ Projeto-ByteBank/index.js
@@ -2,24 +2,46 @@
 
 class Cliente{
     nome;
-    cpf;
+    cpf;    
+}
+
+class ContaCorrente{
+    // https://github.com/tc39/proposal-class-fields#private-fields
+    // "#"  representa uma propriedade privada 
+    // "_" representa apenas uma convenção da comunidde de que a propriedade é privada, porém, não há bloqueio efetivo do uso externo
     agencia;
-    saldo;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0){
+            return;
+        }
+        this._saldo += valor;            
+    }
 }
 
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
-
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001;
-cliente2.saldo = 0;
 
+const contaCorrenteRicardo = new ContaCorrente();
 
-console.log(cliente1, cliente2);
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(300);
+const valorSacado = contaCorrenteRicardo.sacar(50);
+
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
